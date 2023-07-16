f902a06671ca4ac863903dc990e336a4cf1ae0fa
allan <allanmathenge2@gmail.com>
2023-07-16 09:07:54 +0300

    first commit

--- server.js
+++ server.js
@@ -1,20 +1,36 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const { logger } = require('./middleware/logger');
+const errorHandler = require('./middleware/errorHandler');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const corsOptions = require('./config/corsOptions')
 const PORT = process.env.PORT || 3500;
 
-app.use('/', express.static(path.join(__dirname, '/public')));
+app.use(logger);
+
+app.use(cors(corsOptions));
+
+app.use(express.json()); //ability to process json
+
+app.use(cookieParser());
+
+app.use('/', express.static(path.join(__dirname, 'public'))); //app.use(express.static( 'public')) would also work
 
 app.use('/', require('./routes/root'));
 
 app.all('*', (req, res) => {
     res.status(404);
     if(req.accepts('html')){
-        res.sendFile(path.join(__dirname, 'views', '404.html'))
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
     } else if (req.accepts('json')){
-        res.json({ Message: '404 Not Found!'})
+        res.json({ message: '404 Not Found!'});
     } else {
-        res.type('txt').send('404 Not Found')
+        res.type('txt').send('404 Not Found');
     }
-})
+});
+
+app.use(errorHandler);
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
